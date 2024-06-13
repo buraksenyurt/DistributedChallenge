4795d69a78d2fb2037c82fe09c01670ab8c07743
buraksenyurt <burakselimsenyurt@gmail.com>
2024-06-13 17:30:07 +0300

    Refactored shell scripts

---
+++ add_secrets.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export AWS_ACCESS_KEY_ID=test
+export AWS_SECRET_ACCESS_KEY=test
+export AWS_DEFAULT_REGION=eu-west-1
+
+add_secret() {
+    secret_name=$1
+    secret_value=$2
+    aws --endpoint-url=http://localhost:4566 secretsmanager create-secret --name $secret_name --secret-string $secret_value
+    echo "Added secret $secret_name"
+}
+
+add_secret "RedisConnectionString" "localhost:6379"
+add_secret "EvalServiceApiAddress" "localhost:5147/api"
+add_secret "HomeGatewayApiAddress" "localhost:5102"
---
+++ get_secrets.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+export AWS_ACCESS_KEY_ID=test
+export AWS_SECRET_ACCESS_KEY=test
+export AWS_DEFAULT_REGION=eu-west-1
+
+get_secret() {
+    secret_name=$1
+    secret_value=$(aws --endpoint-url=http://localhost:4566 secretsmanager get-secret-value --secret-id $secret_name --query SecretString --output text)
+    echo "$secret_name: $secret_value"
+}
+
+get_secret "RedisConnectionString"
+get_secret "EvalServiceApiAddress"
+get_secret "HomeGatewayApiAddress"
