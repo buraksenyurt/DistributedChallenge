2289a65e843009ff5af9d1ea9c23142e2646cde3
buraksenyurt <burakselimsenyurt@gmail.com>
2024-06-26 20:23:07 +0300

    Updated the table design.

--- DbScripts.sql
+++ DbScripts.sql
@@ -1,6 +1,7 @@
 CREATE TABLE Documents (
     Id SERIAL PRIMARY KEY,
     TraceId UUID NOT NULL,
+    EmployeeId VARCHAR(20) NOT NULL,
     DocumentId VARCHAR(255) NOT NULL,
     Content BYTEA NOT NULL
 );
--- SystemHome/GamersWorld.Repository/DocumentRepository.cs
+++ SystemHome/GamersWorld.Repository/DocumentRepository.cs
@@ -10,6 +10,7 @@ public class DocumentRepository(ISecretStoreService secretStoreService)
 {
     private readonly ISecretStoreService _secretStoreService = secretStoreService;
 
+    //QUESTION int döndürmek yerine daha anlamlı bir veri yapısı dönülemez mi?
     public async Task<int> InsertDocumentAsync(DocumentSaveRequest documentSaveRequest)
     {
         var connStr = await _secretStoreService.GetSecretAsync("ReportDbConnStr");
@@ -18,19 +19,22 @@ public class DocumentRepository(ISecretStoreService secretStoreService)
         dbConnection.Open();
 
         var sql = @"
-                INSERT INTO Documents (TraceId, DocumentId, Content)
-                VALUES (@TraceId, @DocumentId, @Content)
+                INSERT INTO Documents (TraceId, EmployeeId, DocumentId, Content)
+                VALUES (@TraceId, @EmployeeId, @DocumentId, @Content)
                 RETURNING Id";
 
         var insertedId = await dbConnection.ExecuteScalarAsync<int>(sql, new
         {
             documentSaveRequest.TraceId,
+            documentSaveRequest.EmployeeId,
             documentSaveRequest.DocumentId,
             documentSaveRequest.Content
         });
 
         return insertedId;
     }
+
+    //QUESTION byte[] array döndürmek yerine daha anlamlı bir veri yapısı dönülemez mi?
 
     public async Task<byte[]> ReadDocumentAsync(DocumentReadRequest documentReadRequest)
     {
