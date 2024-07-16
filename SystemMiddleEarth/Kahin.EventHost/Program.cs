842f0eba68ea7a6fdd7825583a24b733fafb8517
Burak Selim Senyurt (Dogus Teknoloji) <BurakSelim.Senyurt@d-teknoloji.com.tr>
2024-07-16 21:01:06 +0300

    Added the retry resilience policy to event host apps.

--- SystemMiddleEarth/Kahin.EventHost/Program.cs
+++ SystemMiddleEarth/Kahin.EventHost/Program.cs
@@ -3,6 +3,9 @@ using Kahin.EventHost;
 using Kahin.MQ;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Http.Resilience;
+using Microsoft.Extensions.Logging;
+using Polly;
 using SecretsAgent;
 using Steeltoe.Common.Http.Discovery;
 using Steeltoe.Discovery.Client;
@@ -14,17 +17,35 @@ var host = Host.CreateDefaultBuilder(args)
         services.AddSingleton<IRedisService, RedisService>();
         services.AddHostedService<Worker>();
         services.AddDiscoveryClient();
-        services.AddHttpClient<HomeGatewayServiceClient>(client =>
+        services.AddHttpClient<HomeGatewayServiceClient>("HomeGatewayService", client =>
         {
             client.BaseAddress = new Uri("http://home-gateway-service");
         })
         .AddServiceDiscovery()
-        .AddRoundRobinLoadBalancer();
+        .AddRoundRobinLoadBalancer()
+        .AddResilienceHandler("Resilience Pipeline",
+            static builder =>
+            {
+                builder.AddRetry(new HttpRetryStrategyOptions
+                {
+                    BackoffType = DelayBackoffType.Exponential,
+                    MaxRetryAttempts = 5,
+                    Delay = TimeSpan.FromSeconds(3),
+                    UseJitter = true
+                });
+            });
+
         services.AddSingleton(context.Configuration);
     })
     .Build();
 
-Console.WriteLine("Press any key to start Host App");
-Console.ReadLine();
+var logger = host.Services.GetRequiredService<ILogger<Program>>();
+
+var httpClientFactory = host.Services.GetRequiredService<IHttpClientFactory>();
+var httpClient = httpClientFactory.CreateClient("HomeGatewayService");
+await ServiceController.IsReportingServiceAlive(httpClient, logger);
+
+//Console.WriteLine("Press any key to start Host App");
+//Console.ReadLine();
 
 await host.RunAsync();
