0b483e6107484c9e2a025c641f27ecc1a9a8db41
Burak Selim Senyurt (Dogus Teknoloji) <BurakSelim.Senyurt@d-teknoloji.com.tr>
2024-07-25 18:33:08 +0300

    Adapted EK to reporting service.

---
+++ SystemMiddleEarth/Kahin.Service.ReportingGateway/LoggingExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Serilog;
+using Serilog.Sinks.Elasticsearch;
+
+namespace Kahin.Service.ReportingGateway;
+
+public static class LoggingExtensions
+{
+    public static LoggerConfiguration UseElasticsearch(this LoggerConfiguration loggerConfiguration, string systemName, string environmentName)
+    {
+        return loggerConfiguration
+            .Enrich.FromLogContext()
+            .Enrich.WithProperty("System", systemName)
+            .Enrich.WithProperty("Environment", environmentName)
+            .WriteTo.Console()
+            .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri("http://localhost:9200"))
+            {
+                AutoRegisterTemplate = true,
+                IndexFormat = $"kahin-reporting-service-logs-{environmentName.ToLower()}",
+                TypeName = null,
+                BatchAction = ElasticOpType.Create,
+                ModifyConnectionSettings = x => x.ServerCertificateValidationCallback((sender, cert, chain, errors) => true)
+            });
+    }
+}
